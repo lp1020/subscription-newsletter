de1fede47881454705709b6de9b1a816aab4ddde
lp1020 <lianapenn@gmail.com>
2020-07-08 00:07:43 -0400

    initial commit

---
+++ src/components/Subscribe/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Subscribe extends Component {
+
+
+    render() {
+        return (
+        	<div>
+        	 <h1>Subscribe!</h1>
+        	 </div>
+        	);
+    }
+
+}
+
+export default Subscribe;
