7e1480ffea6f7af94407c976d43175e5ca26672c
lp1020 <lianapenn@gmail.com>
2020-07-08 18:30:22 -0400

    functionality

--- src/components/NewsletterList/index.js
+++ src/components/NewsletterList/index.js
@@ -1,76 +1,80 @@
 import React, { useState, Component } from "react";
 
-const [toggleState, setToggleState] = useState("off");
- function toggle() {
-    setToggleState(toggleState === "off" ? "on" : "off");
-  }
-
-class NewsletterList extends Component {
 
 
+const NewsletterList = ({ component }) => {
+  const [toggleActive, setToggleState] = useState(false);
+  const [toggleActive2, setToggleState2] = useState(false);
+  const [toggleActive3, setToggleState3] = useState(false);
+  const [toggleActive4, setToggleState4] = useState(false);
 
-    render() {
-        return (
+  return (
           <div className="container">
 
           <div className="row newsletter-row">
 
 
           <div className="col-md-3 col-sm-12">
-          <img src="images/img-blank.png" className="blank-img img-fluid"/>
+          <img src= "./img-blank.png" className="blank-img img-fluid"/>
           </div>
           <div className="col-lg-7 col-sm-12 news-title-wrapper">
           <p className="newsletter-title">Reverse The Online Gambling Ban</p>
           <p className="newsletter-desc">The MPA comes with three different sizes of foam and silicone tips and a carrying pouch.</p>
           </div>
           <div className="col-lg-2 col-sm-12">
-          <div className={`switch ${toggleState}`} onClick={toggle} />
+          <div className={`check-area ${toggleActive ? "checked" : ""}`}
+     onClick={() => setToggleState(!toggleActive)}
+     />
           </div>
           </div>
 
 
           <div className="row newsletter-row">
             <div className="col-md-3 col-sm-12">
-          <img src="images/img-blank.png" className="blank-img img-fluid"/>
+          <img src="./img-blank.png" className="blank-img img-fluid"/>
           </div>
           <div className="col-lg-7 col-sm-12 news-title-wrapper">
           <p className="newsletter-title">Successful Myspace Profile</p>
           <p className="newsletter-desc">All users on MySpace will know that there are millions of people out there.</p>
           </div>
           <div className="col-lg-2 col-sm-12">
-          <img src="images/uncheck-icon.png" className="uncheck-icon img-fluid"/>
+              <div className={`check-area ${toggleActive2 ? "checked" : ""}`}
+     onClick={() => setToggleState2(!toggleActive2)}
+     />
           </div>
           </div>
 
           <div className="row newsletter-row">
             <div className="col-md-3 col-sm-12">
-          <img src="images/img-blank.png" className="blank-img img-fluid"/>
+          <img src="./img-blank.png" className="blank-img img-fluid"/>
           </div>
           <div className="col-lg-7 col-sm-12 news-title-wrapper">
           <p className="newsletter-title">Home Audio Recording For Everyone</p>
           <p className="newsletter-desc">The number of friends and acquaintances will grow in a large number. This is what you should be looking for.</p>
           </div>
           <div className="col-lg-2 col-sm-12">
-          <img src="images/uncheck-icon.png" className="uncheck-icon img-fluid"/>
+              <div className={`check-area ${toggleActive3 ? "checked" : ""}`}
+     onClick={() => setToggleState3(!toggleActive3)}
+     />
           </div>
           </div>
 
           <div className="row newsletter-row">
             <div className="col-md-3 col-sm-12">
-          <img src="images/img-blank.png" className="blank-img img-fluid"/>
+          <img src="./img-blank.png" className="blank-img img-fluid"/>
           </div>
           <div className="col-lg-7 col-sm-12 news-title-wrapper">
           <p className="newsletter-title">Buying Used Electronic Test Equipment</p>
           <p className="newsletter-desc">People after all will make friends after reading about you. If you have an ugly profile, there is no way th</p>
           </div>
           <div className="col-lg-2 col-sm-12">
-          <img src="images/uncheck-icon.png" className="uncheck-icon img-fluid"/>
+              <div className={`check-area ${toggleActive4 ? "checked" : ""}`}
+     onClick={() => setToggleState4(!toggleActive4)}
+     />
           </div>
           </div>
           </div>
           );
     }
-
-}
 
 export default NewsletterList;
--- src/components/Subscribe/index.js
+++ src/components/Subscribe/index.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import { useForm } from 'react-hook-form';
 
-class Subscribe extends Component {
+export default function Subscribe() {
+
+const {register, handleSubmit, errors} = useForm();
+
+const onSubmit = (data) => {
+  setTimeout(() => alert('You are now subscribed!'), 2000)
+  console.log(data);
+}
 
 
-    render() {
         return (
         	<div className="subscribe-wrapper">
         	 <div className="container">
            <div className="row">
            <div className="col-12 text-center">
-           <form>
-           <input className="email-input" type="text" placeholder="Enter email" name="email" />
+           <form onSubmit={handleSubmit(onSubmit)}>
+           {errors.email && <p className="red-text">{errors.email.message}</p>}
+           <input className="email-input" type="text" placeholder="Enter email" name="email" ref={register ({required: "Field is blank", minLength: { value: 6, message: "Please use valid e-mail"} })} />
            <input className="email-submit mx-auto" type="submit" value="SUBSCRIBE" />
            <input type="checkbox" id="checkbox" name="checkbox" value="checkbox"/>
-            <label for="checkbox"> I do not want to receive information about future newsletters.</label>
+            <label htmlFor="checkbox"> I do not want to receive information about future newsletters.</label>
            </form>
            </div>
            </div>
@@ -22,6 +30,5 @@ class Subscribe extends Component {
         	);
     }
 
-}
 
-export default Subscribe;
+
